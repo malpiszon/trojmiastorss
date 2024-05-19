81a0a8b769160ddb1afb589fd713f6cdde440cfe
Alan <alan@malpiszon.net>
2024-05-19 19:00:43 +0200

    Update tromiasto.pl reader due to CSS changes

--- trojmiastorss.py
+++ trojmiastorss.py
@@ -34,35 +34,35 @@ def lambda_handler(event, context):
         lastItemPubDate = int(lastItem['Items'][0]['pubDate'])
 
     items = 0
-    for art in articlesSoup.find_all('li', class_='arch-item'):
-        dateOpinions = art.find('span', class_='op-list')
-        dateText = dateOpinions.text.strip().split('\xa0')[0].strip()
+    for art in articlesSoup.find_all('article', class_='newsList__article'):
+        dateOpinions = art.find('div', class_='newsList__details')
+        dateText = dateOpinions.find('span', class_='newsList__date').text.strip()
         dateTime = dateparser.parse(dateText, languages=['pl'])
-        artTimestmap = int(dateTime.timestamp())
-        if artTimestmap <= lastItemPubDate:
+        artTimestamp = int(dateTime.timestamp())
+        if artTimestamp <= lastItemPubDate:
             break
         
-        categoryA = art.find('div', class_='category').find('a')
+        categoryA = art.find('div', class_='newsList__tag').find('a')
         category = ''
         if categoryA != None:
             category = categoryA.text.strip()
         if category.lower() not in SKIPPED_CATEGORIES:
-            url = art.find('a').get('href')
+            url = art.find('h4', class_='newsList__title').find('a').get('href')
             artId = int(url[-11:-5])
-            title = art.find('a', class_='color04').text.strip()
-            opinionsText = '' if dateOpinions.find('strong') == None else ', ' + dateOpinions.find('strong').text + ' opinii'
-            notSponsored = art.find('h4').find('i', class_='trm-news-art-sponsorowany') == None
+            title = art.find('h4', class_='newsList__title').find('span', class_='newsList__text').text.strip()
+            opinionsText = '' if dateOpinions.find('b') == None else ', ' + dateOpinions.find('b').text + ' opinii'
+            notSponsored = art.find('h4', class_='newsList__title').find('i', class_='trm-news-art-sponsorowany') == None
             sponsoredText = '' if notSponsored else ', SPONSOROWANY'
             author = '?'
-            description = art.find('div', class_='lead').text.strip()
-            artTtl = artTimestmap + int(TTL)
-            artDateId = artTimestmap*1000000+artId
+            description = art.find('div', class_='newsList__content').find('p', class_='newsList__desc').text.strip()
+            artTtl = artTimestamp + int(TTL)
+            artDateId = artTimestamp*1000000+artId
             if notSponsored:
                 articleFull = http.request('GET', url)
                 articleFullSoup = BeautifulSoup(articleFull.data, 'lxml')
-                authorSpan = articleFullSoup.find('span', class_='article-author')
-                if authorSpan != None:
-                    author = authorSpan.find('strong').text
+                authorDiv = articleFullSoup.find('div', class_='newsHeader__author')
+                if authorDiv != None:
+                    author = authorDiv.text.strip()
                 descriptionP = articleFullSoup.find('p', class_='lead')
                 if descriptionP != None:
                     description = descriptionP.text
@@ -77,7 +77,7 @@ def lambda_handler(event, context):
                     'author': author,
                     'summary': category + opinionsText + sponsoredText,
                     'category': category,
-                    'pubDate': artTimestmap,
+                    'pubDate': artTimestamp,
                     'ttl': artTtl
                 }
             )
